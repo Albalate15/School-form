280c79a714d3090eb83b649fb5503f3df3a9fb05
Andre Albalate <100637805+Albalate15@users.noreply.github.com>
2023-08-25 18:45:31 +0800

    Create signup.php

---
+++ signup.php
@@ -0,0 +1,17 @@
+<?php include("db_config.php"); ?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Sign Up</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+    <div class="container">
+        <h1>Sign Up</h1>
+        <form action="signup.php" method="post">
+            <!-- Your signup form fields here -->
+            <input type="submit" name="signup" value="Sign Up">
+        </form>
+    </div>
+</body>
+</html>
