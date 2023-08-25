0b34ff8abc6757d7affd89ce78a07f9f2c874257
Andre Albalate <100637805+Albalate15@users.noreply.github.com>
2023-08-25 18:44:35 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php include("db_config.php"); ?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>School Website</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+    <div class="container">
+        <h1>Welcome to Our School</h1>
+        <div class="box">
+            <a href="signup.php">Sign Up</a>
+        </div>
+        <div class="box">
+            <a href="login.php">Log In</a>
+        </div>
+    </div>
+</body>
+</html>
