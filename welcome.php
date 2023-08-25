0c499f79a928cae0e81ae6cb3220efd08f7cf2b4
Andre Albalate <100637805+Albalate15@users.noreply.github.com>
2023-08-25 18:44:17 +0800

    Create welcome.php

---
+++ welcome.php
@@ -0,0 +1,14 @@
+<?php include("db_config.php"); ?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Welcome</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+    <div class="container">
+        <h1>Welcome to Our School</h1>
+        <!-- Your welcome content here -->
+    </div>
+</body>
+</html>
