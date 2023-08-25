818ae90433127bc3d421e44679c9abdede0dd0f4
Andre Albalate <100637805+Albalate15@users.noreply.github.com>
2023-08-25 18:45:05 +0800

    Create login.php

---
+++ login.php
@@ -0,0 +1,17 @@
+<?php include("db_config.php"); ?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Log In</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+    <div class="container">
+        <h1>Log In</h1>
+        <form action="login.php" method="post">
+            <!-- Your login form fields here -->
+            <input type="submit" name="login" value="Log In">
+        </form>
+    </div>
+</body>
+</html>
